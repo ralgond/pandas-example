752534c0688141e51dca17418fe7347c8d850946
Teng Huang <ht201509@163.com>
2024-03-30 11:02:46 +0800

    fix bug

--- code/interview041.py
+++ code/interview041.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 t1 = pd.read_excel("../data/c6/14连续访问记录.xlsx", sheet_name="访问记录表")
 
-t1 = t1.sort_values(by=["用户ID", "访问的页面", "访问页面时间"])
+#t1 = t1.sort_values(by=["用户ID", "访问的页面", "访问页面时间"])
+t1 = t1.sort_values(by=["访问页面时间"])
 
 t1["上一个访问的页面"] = t1.groupby("用户ID")["访问的页面"].shift(1)
 
